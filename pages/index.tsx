e2ac51a7369ee9e9a35a0755a53cb7543fd7ece9
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-03-08 08:11:15 -0600

    Basic structure Login/Logout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useUser } from '@auth0/nextjs-auth0/client'
+import Link from 'next/link'
 
 function index() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (error) {
+    return <div>{error.message}</div>
+  }
+  if (user) {
+    return <div>
+      <p>Welcome {user.name}!</p>
+      <Link href="/api/auth/logout">Logout</Link>
+      
+    </div>
+  }
   return (
-    <div>index</div>
+    <Link href="/api/auth/login">Login</Link>
   )
 }
 
